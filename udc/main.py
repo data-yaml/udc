a1b84f4be03aa8a73d131ef95cc90e0bb3359080
Ernest Prabhakar <ernest@quiltdata.io>
2023-05-12 12:49:57 -0700

    echo URI

--- udc/main.py
+++ udc/main.py
@@ -2,10 +2,15 @@ import anyio
 import asyncclick as click
 
 @click.command()
-@click.argument("name")
-async def app(name, **kwargs):
-    """Simple program that greets NAME for a total of COUNT times."""
-    click.echo(f"Hello, {name}!")
+@click.pass_context
+@click.argument("uri")
+async def app(ctx, uri, **kwargs):
+    """Simple program that echos URI."""
+    click.echo(f"URI: {uri}")
+
+
+def main():
+    app(_anyio_backend="trio")  # or asyncio
 
 if __name__ == '__main__':
-    app(_anyio_backend="trio")  # or asyncio
+    main()
