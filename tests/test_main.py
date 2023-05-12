d38ba8daaa7c71bd2ade18dbba9e336915023237
Ernest Prabhakar <ernest@quiltdata.io>
2023-05-12 12:48:34 -0700

    single argument

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,7 +13,7 @@ runner = CliRunner()
 TEST_URI = "quilt+s3://quilt-example"
 
 async def test_list():
-    result = await runner.invoke(app, [TEST_URI, "1"])
+    result = await runner.invoke(app, [TEST_URI])
     print(result)
     assert result.exit_code == 0
     assert TEST_URI in result.stdout
--- udc/main.py
+++ udc/main.py
@@ -3,13 +3,9 @@ import asyncclick as click
 
 @click.command()
 @click.argument("name")
-@click.argument("count", type=int)
-async def app(name, count, **kwargs):
+async def app(name, **kwargs):
     """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        if x:
-            await anyio.sleep(0.1)
-        click.echo(f"Hello, {name}!")
+    click.echo(f"Hello, {name}!")
 
 if __name__ == '__main__':
     app(_anyio_backend="trio")  # or asyncio
